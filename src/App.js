84d0b9156c316b625175b6f44d244192287f34c0
tscotce <tatyana.celovsky@gmail.com>
2021-09-17 17:12:22 -0400

    Fetch images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-function App() {
+const BASE_URL = "https://api.nasa.gov/planetary/apod?api_key="
+const QUERY_URL = `${BASE_URL}${process.env.REACT_APP_NASA_API_KEY}`
+
+class App extends React.Component { 
+
+  componentDidMount() {
+    this.fetchImages()
+  }
+
+  fetchImages = () => {
+    const configObj = {
+      method: "GET",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      }
+    }
+    return fetch(QUERY_URL, configObj)
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
+  render() {
   return (
     <div className="App">
       <Header />
       <Footer />
     </div>
   );
+  }
 }
 
 export default App;
