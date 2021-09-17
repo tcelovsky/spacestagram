c28f796807b86cab55750a9eed4b1960f90c50eb
tscotce <tatyana.celovsky@gmail.com>
2021-09-17 16:09:47 -0400

    Update fetchImages

--- src/actions/fetchImages.js
+++ src/actions/fetchImages.js
@@ -3,14 +3,14 @@ const QUERY_URL = `${BASE_URL}/launches`
 
 export const fetchImages = () => {
     return () => {
-    //   dispatch({ type: 'LOADING_LAUNCHES'})
+      dispatch({ type: 'LOADING_IMAGES'})
       fetch(QUERY_URL)
         .then(response => {
           return response.json()
         })
         .then(responseJSON => {
-        //   dispatch({ type: 'ADD_LAUNCHES', launches: responseJSON })
-          console.log(responseJSON)
+          dispatch({ type: 'ADD_IMAGES', images: responseJSON })
+        //   console.log(responseJSON)
         })
     }
   }
