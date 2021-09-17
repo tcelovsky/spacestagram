501d1b77aae1e2f0dcd71a996a0d6b43a2f9d678
tscotce <tatyana.celovsky@gmail.com>
2021-09-17 17:44:17 -0400

    Move fetching logic to Main

--- src/App.js
+++ src/App.js
@@ -4,27 +4,7 @@ import Main from './components/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-const BASE_URL = "https://images-api.nasa.gov/search?"
-const QUERY_URL = `${BASE_URL}q=earth%20from%20space&media_type=image`
-
 class App extends React.Component { 
-
-  componentDidMount() {
-    this.fetchImages()
-  }
-
-  fetchImages = () => {
-    const configObj = {
-      method: "GET",
-      headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-      }
-    }
-    return fetch(QUERY_URL, configObj)
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }
 
   render() {
   return (
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+const BASE_URL = "https://images-api.nasa.gov/search?"
+const QUERY_URL = `${BASE_URL}q=earth%20from%20space&media_type=image`
 
 const Main = () => {
+
+    useEffect(() => {
+        let isMounted = true;
+        if (isMounted) {
+          fetchImages();
+        }
+        return () => {
+          isMounted = false;
+        };
+      }, []);
+
+    const  fetchImages = () => {
+        const configObj = {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+          }
+        }
+        return fetch(QUERY_URL, configObj)
+          .then(response => response.json())
+          .then(data => console.log(data))
+      }
+
     return (
       <main>
           Main
