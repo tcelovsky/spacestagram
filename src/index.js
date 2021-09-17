e1cf39a4aae0464d41e68bdd0839fea82bb30078
tscotce <tatyana.celovsky@gmail.com>
2021-09-17 16:32:13 -0400

    Update App.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import imagesReducer from './reducers/imagesReducer';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(imagesReducer, applyMiddleware(thunk));
-
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
