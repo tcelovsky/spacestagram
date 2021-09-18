e65066941cd446baa34dedaf2166bdcfda3d0bdc
tscotce <tatyana.celovsky@gmail.com>
2021-09-18 16:37:03 -0400

    Update cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,11 @@ const Card = (item) => {
     
     return (
       <figure>
-          <figcaption>{item.item.data[0].title}</figcaption>
-          <img src={item.item.links[0].href} alt={item.item.data[0].title} />
-          <div>{item.item.data[0].date_created}</div>
-          <div>{item.item.data[0].description}</div>
+          <figcaption>{item.item.title}</figcaption>
+          <img src={item.item.url} alt={item.item.title} />
+          <div>{item.item.copyright}</div>
+          <div>{item.item.date}</div>
+          <div>{item.item.explanation}</div>
       </figure>
     );
   }
