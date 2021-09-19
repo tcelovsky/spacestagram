8ce5cf3b42bff1cfb699b470e61886cca40e0c64
tscotce <tatyana.celovsky@gmail.com>
2021-09-19 13:11:22 -0400

    Create Like button

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,10 @@
+import Like from './Like';
+
 const About = () => {
     return (
       <div className="about">
           <p>This website is a compilation of the current month's Astronomy Picture of the Day from NASA.</p>
+          <Like />
       </div>
     );
   }
