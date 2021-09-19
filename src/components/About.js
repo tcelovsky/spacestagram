969128d366c776279a8f5af0766b88d194f87438
tscotce <tatyana.celovsky@gmail.com>
2021-09-19 13:45:39 -0400

    Update About

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,8 @@
 const About = () => {
     return (
       <div className="about">
-          <p>This website is a compilation of the current month's Astronomy Picture of the Day from NASA.</p>
+          <p>This website is a compilation of the current month's Astronomy Picture of the Day from NASA, which features a daily image or photograph of the universe, along with a brief explanation written by a professional astronomer.
+          </p>
       </div>
     );
   }
