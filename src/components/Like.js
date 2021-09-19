8535a9b0a437b56e6ad3787d50f132f2763ba063
tscotce <tatyana.celovsky@gmail.com>
2021-09-19 12:52:59 -0400

    Create basic Like.js component

---
+++ src/components/Like.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeartBroken } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Like = () => {
+   
+}
+  
+export default Like;
