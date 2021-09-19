f4f993926221796f1dddf3f76ee3cc5ed439a459
tscotce <tatyana.celovsky@gmail.com>
2021-09-18 20:05:41 -0400

    Create About component

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+      <div>
+          <p>This website is a compilation of the current month's Astronomy Picture of the Day from NASA.</p>
+      </div>
+    );
+  }
+  
+  export default About;
