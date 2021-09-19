e7c6a472ad9ab859bbb8103ce132d53ace679237
tscotce <tatyana.celovsky@gmail.com>
2021-09-18 20:08:28 -0400

    Add React Router and Switch to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './styles/App.css';
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Main from './components/Main';
+import About from './components/About';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -10,7 +12,10 @@ class App extends React.Component {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Switch>
+        <Route exact path='/' component={Main} /> 
+        <Route path='/about' component={About} />
+      </Switch>
       <Footer />
     </div>
   );
