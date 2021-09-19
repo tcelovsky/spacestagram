ee4828ee34f6ca5b4413bcf7e6360073d7e4f432
tscotce <tatyana.celovsky@gmail.com>
2021-09-19 13:25:58 -0400

    Update like button styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,7 @@ const Card = (item) => {
           <figcaption>{item.item.title}</figcaption>
           <img src={item.item.url} alt={item.item.title} />
           <p className="copyright">{item.item.copyright}</p>
-          <p className="date">{item.item.date}</p>
-          <Like />
+          <p className="date">{item.item.date}<Like /></p>
           <p className="explanation">{item.item.explanation}</p>
       </figure>
     );
