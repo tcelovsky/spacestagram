553a13e488df3309f43179d76c4447d54fe82a81
tscotce <tatyana.celovsky@gmail.com>
2021-09-19 14:07:34 -0400

    Update styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,8 @@ const Card = (item) => {
           <figcaption>{item.item.title}</figcaption>
           <img src={item.item.url} alt={item.item.title} />
           <p className="copyright">{item.item.copyright}</p>
-          <p className="date">{item.item.date}<Like /></p>
+          <p className="date">{item.item.date}</p>
+          <Like />
           <p className="explanation">{item.item.explanation}</p>
       </figure>
     );
